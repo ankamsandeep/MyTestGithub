f7729d1b79f576dc6ca82c830c58f9a80b3db332
Sandeep Ankam <sandeepankam@me.com>
2016-07-11 20:40:25 -0700

    Added comments

--- TestGitHub/ViewController.m
+++ TestGitHub/ViewController.m
@@ -16,7 +16,9 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    // THis is a test comment.
 }
 
 - (void)didReceiveMemoryWarning {
